91ad3aa418a8e9a83f5869d943f6f01b335d916e
NamJungGu <nowage@gmail.com>
2019-03-30 19:51:15 +0900

    Changed region & ami Changes to be committed:     modified:   first-steps/instance.tf

--- first-steps/instance.tf
+++ first-steps/instance.tf
@@ -1,10 +1,10 @@
 provider "aws" {
   access_key = "ACCESS_KEY_HERE"
   secret_key = "SECRET_KEY_HERE"
-  region     = "us-east-1"
+  region     = "ap-northeast-2"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0d729a60"
+  ami           = "ami-79815217"
   instance_type = "t2.micro"
 }
